03b27984657f524e4059f01e25a754568930d8bd
Ommps246 <ommps246@gmail.com>
2025-12-10 11:44:27 +0530

    Add files via upload

---
+++ tute dude task 2.py
@@ -0,0 +1,7 @@
+n = int(input("Enter your number"))
+if n % 2 == 0:
+    print(f"{n} is Even number")
+elif n%2 != 0:
+    print(f"{n} is Odd number")
+else:
+    print("enter valid data")
---
+++ tutedude ass2 task 1.py
@@ -0,0 +1,5 @@
+num = 51
+j = 0
+for i in range (num):
+    j = j+i
+print("The num of numbers from 1 to 50 is :",j)
